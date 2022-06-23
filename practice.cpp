36ee219fcbf1d67a58f3fda031548cc8959ca749
fazil <67891255+binary01-spec@users.noreply.github.com>
2022-06-23 07:25:41 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,2 +1,14 @@
 //var format = /[a-z0-9]+@[a-z]{2,3}$/;
-
+ /*<script type="text/javascript">
+      function validation() {
+        var form = document.getElementById("form");
+        var email = document.getElementById("email").value;
+        var pass = document.getElementById("pass").value;
+        var format = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+        if (email.match(format)) {
+          alert("login success");
+        } else {
+          alert("login failed");
+        }
+      }
+    </script>*/
